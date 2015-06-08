cc6bf37939243ee817c7bc074b38e936717b11bb
AdamRGrey <adam.r.grey@gmail.com>
2015-06-07 20:50:17 -0400

    initial

---
+++ WatchAndMute/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreAudio;
+using CoreAudio.Interfaces;
+
+namespace WatchAndMute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
